74e9570e99284c4b2e98d5e8b0495466f937594a
xiao <gds1105928109@qq.com>
2020-05-25 20:58:30 +0800

    登录功能:增加默认账号密码判断

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.helloworld.databinding.ActivityMainBinding;
 
@@ -22,8 +23,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //intent:意图分为是显示和隐式
                 //new Intent的参数上下文,目标activity的类
-                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(intent);
+                String phone=mBinding.editPhone.getText().toString();
+                String pwd=mBinding.editPwd.getText().toString();
+                if(phone.equals("10086")&&pwd.equals("123456")){
+                    Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this,"手机号或密码错误",Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
