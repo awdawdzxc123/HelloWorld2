6a455b4959f36630f4add627846534f6a71c0cb9
xiao <gds1105928109@qq.com>
2020-05-25 20:52:32 +0800

    实现功能:点击登录按钮跳转至HomeActivity

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.helloworld.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        mBinding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //intent:意图分为是显示和隐式
+                //new Intent的参数上下文,目标activity的类
+                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
