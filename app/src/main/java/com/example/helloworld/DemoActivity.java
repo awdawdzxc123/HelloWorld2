0a3f14090ab1bd706bc46a4c257a463355ad6f3c
xiao <gds1105928109@qq.com>
2020-05-29 09:42:17 +0800

    实现功能:注册功能 已实现:注册保存数据逻辑 末实现:注册界面回调登录界面

--- app/src/main/java/com/example/helloworld/DemoActivity.java
+++ app/src/main/java/com/example/helloworld/DemoActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
@@ -42,12 +43,29 @@ public class DemoActivity extends AppCompatActivity  {
                     toast="请确保两次输入密码一致";
                 }else if (phone.length()!=11){
                     toast="请使用正确中国大陆手机号码";
-                }else if (!protocol){
-                    toast="请同意本软件的相关协议和政略,否则无法注册";
+                }else if (!protocol) {
+                    toast = "请同意本软件的相关协议和政略,否则无法注册";
+                }else{
                     //存储该用户的注册数据
-                }else toast="注册成功";
-                    //跳转至登录界面
-                    Toast.makeText(DemoActivity.this,toast,Toast.LENGTH_LONG).show();
+                    //Sharedpreferences存储技术
+                    SharedPreferences sp = getSharedPreferences("user_info", MODE_PRIVATE);
+                    String temp = sp.getString("phone_" + phone, "0");//如果是0，说明没有找到这个手机
+                    if (!temp.equals("0")) {//如果不是0，则表示该手机号码是否已被注册
+                        toast = "该数据号码也被注册";
+                    } else {
+                        SharedPreferences.Editor editor = sp.edit();
+                        editor.putString("phone_" + phone, phone);
+                        editor.putString("name_" + phone, name);
+                        editor.putString("sex_" + phone, sex);
+                        editor.putString("pwd" + phone, pwd);
+                        temp =(sms?"1":"0");
+                        editor.putString("sms_" + phone, temp);
+                        editor.apply();
+                        toast = "注册成功";
+                        //跳转至登录界面
+                    }
+                }
+                Toast.makeText(DemoActivity.this,toast,Toast.LENGTH_LONG).show();
             }
         });
         mBinding.radioGroupSex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
