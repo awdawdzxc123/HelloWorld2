c217c46acd81ef29ac0776abeb4b03a044be3928
xiao <gds1105928109@qq.com>
2020-06-07 16:31:13 +0800

    HomeActivity实现功能:快速双击退出键,退出,登录

--- app/src/main/java/com/example/helloworld/HomeActivity.java
+++ app/src/main/java/com/example/helloworld/HomeActivity.java
@@ -6,11 +6,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.helloworld.databinding.ActivityHomeBinding;
 
+import java.util.Objects;
+
 public class HomeActivity extends AppCompatActivity {
     private ActivityHomeBinding mBinding;
+    private long exitTime=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,16 +22,38 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(mBinding.getRoot());
 
         Intent intent=getIntent();//获取传过来的Intent对象
-        String phone=intent.getStringExtra("data_phone");//取出对应的值
-        mBinding.userPhone.setText(phone);
+        //接受转过来的Intent对象
+        UserInfo u=(UserInfo) intent.getSerializableExtra("userIntfo");
+        //取出对应的值
+        String  Phone  = Objects.requireNonNull(u).getShone();
+        String userName =u.getUserName();
+        String userSex =u.getSex();
+        String userSms =u.getSms();
 
-        SharedPreferences sp = getSharedPreferences("user_info", MODE_PRIVATE);
-        String userName = sp.getString("name_" + phone, "0");
-        String userSex = sp.getString("sex_" + phone, "0");
-        String userSms = sp.getString("sms_" + phone, "0").equals("1")?"接受":"不接受";
+        mBinding.userPhone.setText(Phone);
         mBinding.userName.setText(userName);
-        mBinding.userName.setText(userSex);
+        mBinding.userSex.setText(userSex);
         String temp =mBinding.userSms.getText().toString()+":"+userSms;
         mBinding.userSms.setText(temp);
+    }
+
+    /**
+     * 拦截系统返回键
+     */
+    @Override
+    public void onBackPressed() {
+        exit();
+
+    }
+    private void exit(){
+        long time=2000;
+        if (System.currentTimeMillis() - exitTime > time){
+            //存储此次点击返回键的时间
+            exitTime=System.currentTimeMillis();
+            Toast.makeText(getApplicationContext(),"快速点击两次,退出当前账号",Toast.LENGTH_SHORT).show();
+        }else{
+            //完全退出APP
+            finish();
+        }
     }
 }
